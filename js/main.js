8253ad9ce9e3093bd285ccc2fb0139272e478fcc
David Meincke <dmeincke@gmail.com>
2017-10-30 18:19:08 -0400

    a bunch of CSS grid experiments

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function getDate(timestamp) {
-	var d = new Date(timestamp);
-	return d
+  var d = new Date(timestamp);
+  return d.toDateString()
 }
 
 Vue.component('display-footer', {
-	props: ['datetext'],
-	template: `
+  props: ['datetext'],
+  template: `
   <section id='bottom-section'>
 	  <div id='bottom-grey'>
 	   <span id="date-time">{{datetext}}</span>
@@ -19,11 +19,10 @@ Vue.component('display-footer', {
 
 
 Vue.component('display-header', {
-	template: `
+  template: `
 	 <section id ='main-header'>
 	 <img src='static/library-identifier-whiteandorange.svg'>
 
-<span id='library-url'>http://pvd.library.jwu.edu</span>
 </section>
   `
 });
@@ -31,17 +30,19 @@ Vue.component('display-header', {
 
 
 Vue.component('homepage-content', {
-	props: ['datetext'],
-	template: `
-	<div class='display-item-wrapper'>
+  props: ['datetext'],
+  template: `
+
 	<div id='homepage-item' class="display-item">
-		<h2 style="grid-column:2/4;">Visit the Library Homepage</h2>
+	<section class="splash-area">
+		<h2>Visit the Library Homepage</h2>
+		<img   style="grid-column:1/3;" src='static/homepage-view-desktop.png'>
+		<img style="grid-column:3/5;" src='static/homepage-view-mobile.png'>
+		</section>
+		<section class='info-area'>
+		<h4 style="grid-column:1/2;"> You can</h4>
 
-		<img style="grid-column:1/3;" src='static/homepage-view-desktop.png'>
-		<img style="grid-column:3/4;" src='static/homepage-view-mobile.png'>
-		<h4 style="grid-column:1/4;">You can</h4>
-
-		<ul class='horizontal-list' style="grid-column:1/4;">
+		<ul style="grid-column:2/5;" class='horizontal-list'>
 			<li>Find books and articles</li>
 			<li>See our textbook collection</li>
 			<li>Chat with a librarian</li>
@@ -49,24 +50,24 @@ Vue.component('homepage-content', {
 		</ul>
 
 
+
 		<h3 style="grid-column:1/2;">http://pvd.library.jwu.edu/homepage </h3>
 		<span style='grid-column:3/4'>
 									<qriously value='http://pvd.library.jwu.edu/homepage'  :size="100"  /></span>
 
-
-	</div>
+</section>
 	</div>
   `
 });
 
 const vm = new Vue({
-	el: "#app",
-	data() {
-		return {
-			message: "Hello!",
-			date: getDate(_.now())
-		}
-	}
+  el: "#app",
+  data() {
+    return {
+      message: "Hello!",
+      date: getDate(_.now())
+    }
+  }
 });
 
 

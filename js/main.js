d80fc7f5067c2d6348b3c78e36c17dc29c6b8474
David <dmeincke@gmail.com>
2017-11-06 20:41:55 -0500

    Started to break out components

--- js/main.js
+++ js/main.js
@@ -1,60 +1,90 @@
 function getHours() {
 
-  var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  var cat = document.createElement('div');
+	var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+	var cat = document.createElement('div');
 
-  url = 'https://api3.libcal.com/api_hours_today.php?iid=1433&lid=0&format=json&systemTime=0';
+	url = 'https://api3.libcal.com/api_hours_today.php?iid=1433&lid=0&format=json&systemTime=0';
 
-  fetch(proxyUrl + url, {
-      method: "GET"
-    }).then(function(response) {
-      return response.json();
+	fetch(proxyUrl + url, {
+			method: "GET"
+		}).then(function(response) {
+			return response.json();
 
-    })
-    .then(function(text) {
-      vm.outHours = text.locations;
+		})
+		.then(function(text) {
+			vm.outHours = text.locations;
 
-      // locLabel = ['Downcity: ', 'Harborside: ', 'Museum: '];
-      // for (var i = 0; i < hours.length; i++) {
-      // 	l = hours[i];
-      // 	hourString = l.times.hours[0].from + ' - ' + l.times.hours[0].to;
-      // 	vm.outHours += (locLabel[i] + hourString + ' ');
-      //
-      // };
+			// locLabel = ['Downcity: ', 'Harborside: ', 'Museum: '];
+			// for (var i = 0; i < hours.length; i++) {
+			// 	l = hours[i];
+			// 	hourString = l.times.hours[0].from + ' - ' + l.times.hours[0].to;
+			// 	vm.outHours += (locLabel[i] + hourString + ' ');
+			//
+			// };
 
 
-    });
+		});
 
 
 };
 
 
 
+// Lower level components
+
+Vue.component('callout-component', {
+	props: ['externalUrl'],
+	template: `
+    <div class='callout'>
+    <span>{{externalUrl}} </span>
+    <span>
+            <qriously class='qr-code'  :value=externalUrl  :size="100"  /></span>
+            </div>
+`
+});
+
+Vue.component('imagelist-component', {
+	props: ['imagelist'],
+	template: `
+    <div class="image-wrap">
+	<div class='db-cards' v-for='item in imagelist'>
+	<img  :src=item.imgPath>
+	<p> {{item.title}} </p>
+	</div>
+	</div>
+`
+});
+
+
+
+
+// Top level components
+
 
 Vue.component('display-header', function(resolve, reject) {
-  setTimeout(function() {
-    resolve({
-      props: ['hours'],
+	setTimeout(function() {
+		resolve({
+			props: ['hours'],
 
-      data() {
-        return {
-          inspirationMsgs: [
-            'You can do it!',
-            'Today is a great day for research',
-            'Have you talked to a librarian today?',
-            'We have hundreds of thousands of ebooks!'
-          ]
+			data() {
+				return {
+					inspirationMsgs: [
+						'You can do it!',
+						'Today is a great day for research',
+						'Have you talked to a librarian today?',
+						'We have hundreds of thousands of ebooks!'
+					]
 
-        }
-      },
-      computed: {
-        welcomeMsg: function() {
-          return _.sample(this.inspirationMsgs)
-        }
+				}
+			},
+			computed: {
+				welcomeMsg: function() {
+					return _.sample(this.inspirationMsgs)
+				}
 
-      },
+			},
 
-      template: `
+			template: `
 				  <section id ='main-header'>
 				   <img src='static/library-identifier-whiteandorange.svg'>
 
@@ -68,45 +98,50 @@ Vue.component('display-header', function(resolve, reject) {
 
 				  </section>
 			  `
-    })
-  }, 50)
+		})
+	}, 50)
 });
 
 
 
 Vue.component('homepage-content', {
-  data() {
-    return {
-      sectionTitle: 'Visit the Library Homepage',
-      bulletItems: ['Find books and articles', 'See our textbook collection', 'Chat with a librarian', 'Book a study room', 'Learn how to research', ],
-      images: ['static/homepage-view-desktop.png', 'static/homepage-view-mobile.png'],
-      qrUrl: 'http://pvd.library.jwu.edu/homepage'
-    }
-  },
+	data() {
+		return {
+			sectionTitle: 'Visit the Library Homepage',
+			bulletItems: ['Find books and articles', 'See our textbook collection', 'Chat with a librarian', 'Book a study room', 'Learn how to research', ],
+		        imageObjList: [{
+                     'imgPath': 'static/homepage-view-desktop.png',
+                     'title': 'Destkop/Laptop',
+                     'url': ''
+                 },
+                 {
+                     'imgPath': 'static/homepage-view-mobile.png',
+                     'title': 'Mobile',
+                     'url': ''
+                 }
+        ]
+             ,
+			qrUrl: 'http://pvd.library.jwu.edu/homepage'
+		}
+	},
 
-  template: `
+	template: `
 
 	<div id='homepage-item' class="display-item">
 
 		<h2 style="grid-column: 1/9">{{sectionTitle}}</h2>
-	<div class="image-wrap">
-	<img  v-for='item in images' :src=item>
-	</div>
 
-			<h4> You can:</h4>
+        <imagelist-component :imagelist=imageObjList>
+        </imagelist-component>
 
+		<h4> You can:</h4>
 				<ul  class='horizontal-list'>
-				<li v-for='item in bulletItems'>{{item}}</li>
+				            <li v-for='item in bulletItems'>{{item}}</li>
 				</ul>
 
-
-
-			<div style="grid-column: 1/9;" class="callout">
-				<span>http://pvd.library.jwu.edu/homepage </span>
-				<span>
-									<qriously class='qr-code'  value='http://pvd.library.jwu.edu/homepage'  :size="100"  /></span>
-
-			</div>
+		<callout-component :externalUrl=qrUrl style="grid-column: 1/9;">
+		</callout-component>
+        
 		</div>
 	  `
 });
@@ -115,58 +150,58 @@ Vue.component('homepage-content', {
 
 
 Vue.component('database-content', {
-  data() {
-    return {
-      sectionTitle: 'Check out our Databases!',
-      svgBullet: `
+	data() {
+		return {
+			sectionTitle: 'Check out our Databases!',
+			svgBullet: `
 	 ---
 
 			`,
-      bulletItemList: [
-        'Read case studies',
-        'Get company ratios',
-        'Read the New York Times or Wall Street Journals (as many articles as you like!)',
-        'Find scholarly articles',
-        'Read ebooks',
-        'Learn new tech skills',
-        'Find topics for persuasive essays'
-      ],
-      imageObjList: [{
-          'imgPath': 'static/database-image-1.png',
-          'title': 'Sage Business Cases',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-2.png',
-          'title': 'Ebook Central',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-3.png',
-          'title': 'Lynda.com',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-4.png',
-          'title': 'Business Source Complete',
-          'url': ''
-        }
-      ],
-      qrUrl: 'http://pvd.library.jwu.edu/az.php'
-    }
-  },
-  computed: {
-    imageObjs: function() {
-      return _.sampleSize(this.imageObjList, [n = 3])
-    },
+			bulletItemList: [
+				'Read case studies',
+				'Get company ratios',
+				'Read the New York Times or Wall Street Journals (as many articles as you like!)',
+				'Find scholarly articles',
+				'Read ebooks',
+				'Learn new tech skills',
+				'Find topics for persuasive essays'
+			],
+			imageObjList: [{
+					'imgPath': 'static/database-image-1.png',
+					'title': 'Sage Business Cases',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-2.png',
+					'title': 'Ebook Central',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-3.png',
+					'title': 'Lynda.com',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-4.png',
+					'title': 'Business Source Complete',
+					'url': ''
+				}
+			],
+			qrUrl: 'http://pvd.library.jwu.edu/az.php'
+		}
+	},
+	computed: {
+		imageObjs: function() {
+			return _.sampleSize(this.imageObjList, [n = 3])
+		},
 
-    bulletItems: function() {
-      return _.sampleSize(this.bulletItemList, [n = 4])
-    }
-  },
+		bulletItems: function() {
+			return _.sampleSize(this.bulletItemList, [n = 4])
+		}
+	},
 
 
-  template: `
+	template: `
 
 		<div id='databases-item' class="display-item">
 
@@ -190,63 +225,61 @@ Vue.component('database-content', {
 
 
 
-			<div style="grid-column: 1/9;" class="callout">
-				<span>{{qrUrl}} </span>
-				<span>
-								<qriously class='qr-code' :value=qrUrl  :size="100"  /></span>
+                <callout-component :externalUrl=qrUrl style="grid-column: 1/9;">
 
-			</div>
+
+    			</callout-component>
 		</div>
   `
 });
 
 
 Vue.component('newbook-content', {
-  data() {
-    return {
-      sectionTitle: 'New Books in Our Collection',
-      svgBullet: `
+	data() {
+		return {
+			sectionTitle: 'New Books in Our Collection',
+			svgBullet: `
 	 ---
 
 			`,
 
-      imageObjList: [{
-          'imgPath': 'static/database-image-1.png',
-          'title': 'Sage Business Cases',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-2.png',
-          'title': 'Ebook Central',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-3.png',
-          'title': 'Lynda.com',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-4.png',
-          'title': 'Business Source Complete',
-          'url': ''
-        }
-      ],
-      qrUrl: 'http://pvd.library.jwu.edu/az.php',
-      lgHtml: 'placeholder'
-    }
-  },
-  computed: {
-    imageObjs: function() {
-      return _.sampleSize(this.imageObjList, [n = 3])
-    },
+			imageObjList: [{
+					'imgPath': 'static/database-image-1.png',
+					'title': 'Sage Business Cases',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-2.png',
+					'title': 'Ebook Central',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-3.png',
+					'title': 'Lynda.com',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-4.png',
+					'title': 'Business Source Complete',
+					'url': ''
+				}
+			],
+			qrUrl: 'http://pvd.library.jwu.edu/newbooks',
+			lgHtml: 'placeholder'
+		}
+	},
+	computed: {
+		imageObjs: function() {
+			return _.sampleSize(this.imageObjList, [n = 3])
+		},
 
-    bulletItems: function() {
-      return _.sampleSize(this.bulletItemList, [n = 4])
-    }
-  },
+		bulletItems: function() {
+			return _.sampleSize(this.bulletItemList, [n = 4])
+		}
+	},
 
 
-  template: `
+	template: `
 
 		<div id='newbooks-item' class="display-item">
 
@@ -263,12 +296,10 @@ Vue.component('newbook-content', {
 
 
 
-			<div style="grid-column: 1/9;" class="callout">
-				<span>{{qrUrl}} </span>
-				<span>
-								<qriously class='qr-code' :value=qrUrl  :size="100"  /></span>
+    <callout-component :externalUrl=qrUrl style="grid-column: 1/9;">
 
-			</div>
+
+    </callout-component>
 		</div>
   `
 });
@@ -276,69 +307,66 @@ Vue.component('newbook-content', {
 
 
 Vue.component('librarian-content', {
-  data() {
-    return {
-      sectionTitle: 'Featured librarians',
-      svgBullet: `
+	data() {
+		return {
+			sectionTitle: 'Featured librarians',
+			svgBullet: `
 	 ---
 
 			`,
 
-      imageObjList: [{
-          'imgPath': 'static/database-image-1.png',
-          'title': 'Sage Business Cases',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-2.png',
-          'title': 'Ebook Central',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-3.png',
-          'title': 'Lynda.com',
-          'url': ''
-        },
-        {
-          'imgPath': 'static/database-image-4.png',
-          'title': 'Business Source Complete',
-          'url': ''
-        }
-      ],
-      qrUrl: 'http://pvd.library.jwu.edu/az.php',
-      lgHtml: 'placeholder'
-    }
-  },
-  computed: {
-    imageObjs: function() {
-      return _.sampleSize(this.imageObjList, [n = 3])
-    },
+			imageObjList: [{
+					'imgPath': 'static/database-image-1.png',
+					'title': 'Sage Business Cases',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-2.png',
+					'title': 'Ebook Central',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-3.png',
+					'title': 'Lynda.com',
+					'url': ''
+				},
+				{
+					'imgPath': 'static/database-image-4.png',
+					'title': 'Business Source Complete',
+					'url': ''
+				}
+			],
+			qrUrl: 'http://pvd.library.jwu.edu/az.php',
+			lgHtml: 'placeholder'
+		}
+	},
+	computed: {
+		imageObjs: function() {
+			return _.sampleSize(this.imageObjList, [n = 3])
+		},
 
-    bulletItems: function() {
-      return _.sampleSize(this.bulletItemList, [n = 4])
-    }
-  },
-	watch : {
-		lgHtml: function(){
-      console.log("CHANGES");
-      parsedLibrarianContent = parseLgContent(this.lgHtml);
-      console.log(parsedLibrarianContent);
+		bulletItems: function() {
+			return _.sampleSize(this.bulletItemList, [n = 4])
+		}
+	},
+	watch: {
+		lgHtml: function() {
+			console.log("CHANGES");
+			parsedLibrarianContent = parseLgContent(this.lgHtml);
 
-  }
+		}
 	},
 
 
-  template: `
+	template: `
 
 		<div id='librarian-item' class="display-item">
 
 			<h2 style="grid-column: 1/9">{{sectionTitle}}</h2>
-	<div class="image-wrap">
-	<div class='db-cards' v-for='item in imageObjs'>
-	<img  :src=item.imgPath>
-	<p> {{item.title}} </p>
-	</div>
-	</div>
+
+
+    <imagelist-component :imagelist=imageObjs>
+    </imagelist-component>
 
 
 
@@ -356,14 +384,14 @@ Vue.component('librarian-content', {
 
 
 Vue.component('display-footer', {
-  computed: {
-    date: function() {
-      var d = new Date(_.now());
-      return d.toDateString()
-    }
+	computed: {
+		date: function() {
+			var d = new Date(_.now());
+			return d.toDateString()
+		}
 
-  },
-  template: `
+	},
+	template: `
 	<section id='bottom-section'>
 	  <div id='bottom-grey'>
 	   <span id="date-time">{{date}}</span>
@@ -377,58 +405,57 @@ Vue.component('display-footer', {
 });
 
 const vm = new Vue({
-  el: "#app",
-  data() {
-    return {
-      message: "Hello!",
+	el: "#app",
+	data() {
+		return {
+			message: "Hello!",
 
 
-      outHours: ''
+			outHours: ''
 
-    }
-  },
-  libraryName: {
-    message: function() {
-      console.log('CHANGED')
-    }
-  },
-  computed: {
-    libraryName: function() {
-      return _.sample(['Downcity Library', 'Harborside Library'])
-    }
+		}
+	},
+	libraryName: {
+		message: function() {
+			console.log('CHANGED')
+		}
+	},
+	computed: {
+		libraryName: function() {
+			return _.sample(['Downcity Library', 'Harborside Library'])
+		}
 
-  }
+	}
 });
 
 
 //TODO
 function storeGetContent(content, container) {
-  console.log(container);
-  vm.$refs[container].lgHtml = content;
+	vm.$refs[container].lgHtml = content;
 };
 
 function getLgContent(url, container) {
 
-  fetch(url, {
-      method: "GET"
-    }).then(function(response, container) {
+	fetch(url, {
+			method: "GET"
+		}).then(function(response, container) {
 
-      return response.text();
+			return response.text();
 
-    })
-    .then(function(text) {
-      storeGetContent(text, container)
+		})
+		.then(function(text) {
+			storeGetContent(text, container)
 
 
-    });
+		});
 
 
 };
 
 
-function parseLgContent(lgContent){
-  parsed = lgContent;
-  return parsed
+function parseLgContent(lgContent) {
+	parsed = lgContent;
+	return parsed
 }
 
 
@@ -438,21 +465,21 @@ function parseLgContent(lgContent){
 
 // Initialize
 $(document).ready(function() {
-  var contentItems = ['#databases-item', '#homepage-item', '#newbooks-item', '#librarian-item'];
+	var contentItems = ['#databases-item', '#homepage-item', '#newbooks-item', '#librarian-item'];
 
-  _.forEach(contentItems, function(item) {
-    $(item).hide();
-  });
+	_.forEach(contentItems, function(item) {
+		$(item).hide();
+	});
 
-  $(_.sample(contentItems)).show();
-  getHours();
+	$(_.sample(contentItems)).show();
+	getHours();
 	// New book content get
-  getLgContent(
-    'https://lgapi-us.libapps.com/widgets.php?site_id=538&widget_type=8&output_format=1&widget_embed_type=2&guide_id=731798&box_id=16491701&map_id=19425291&content_only=0&config_id=1510000840435',
-    'newbookref');
+	getLgContent(
+		'https://lgapi-us.libapps.com/widgets.php?site_id=538&widget_type=8&output_format=1&widget_embed_type=2&guide_id=731798&box_id=16491701&map_id=19425291&content_only=0&config_id=1510000840435',
+		'newbookref');
 
-// Get librarian content
-		getLgContent(
-			'https://lgapi-us.libapps.com/widgets.php?site_id=538&widget_type=8&output_format=1&widget_embed_type=2&guide_id=731798&box_id=16571535&map_id=19518122&content_only=0&config_id=1510006276756',
-			'librarianref');
+	// Get librarian content
+	getLgContent(
+		'https://lgapi-us.libapps.com/widgets.php?site_id=538&widget_type=8&output_format=1&widget_embed_type=2&guide_id=731798&box_id=16571535&map_id=19518122&content_only=0&config_id=1510006276756',
+		'librarianref');
 });
